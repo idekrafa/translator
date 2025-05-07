70a18edcb57b8dfc977e4fdeec94a53b419b300e
Rafael Ribeiro <125091942+idekrafa@users.noreply.github.com>
2025-05-07 14:21:55 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,11 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Use Render's provided port, fallback to 8000 locally
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True,
-    ) 
+        port=port,
+        reload=False  # Turn off reload on production
+    )
